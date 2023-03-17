eeb5fb0faea63982cc2f4fe5cddd548c179574d8
fatma Gelil <gelilfatma79@gmail.com>
2023-03-17 12:34:30 +0200

    2nd

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 require'functions.php';
-require 'router.php';
+//require 'router.php';
+
+class person
+{
+   public $name;
+  public  $age;
+  public function breathe()
+  {
+      echo $this->name.' is breathing';
+  }
+}
+   $person=new person();
+   $person->name='fatma';
+   $person->age=18;
+   dd($person->breathe());
+
+
