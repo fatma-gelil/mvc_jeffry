1623573d56394491ca5cff3f8f5b98036e5a3456
fatma Gelil <gelilfatma79@gmail.com>
2023-03-17 14:09:22 +0200

    2nd

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require'functions.php';
 //require 'router.php';
 
-class person
+/*class person
 {
    public $name;
   public  $age;
@@ -14,6 +14,12 @@ class person
    $person=new person();
    $person->name='fatma';
    $person->age=18;
-   dd($person->breathe());
+   dd($person->breathe());*/
+$dsn="mysql:host=localhost;dbname=week2;charset=utf8mb4";
+$pdo= new PDO($dsn);
+$pdo->prepare("select * from student;");
+$statement->excute();
+$student=$statement->fetchAll();
+dd($student);
 
 
